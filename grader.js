06b0094932ede96cda6379228f995335fe53138f
Yash Vashishth <justyash@gmail.com>
2013-07-16 22:02:44 +0000

    added additional required classes to check in checks.json and modified grader to accept urls

--- grader.js
+++ grader.js
@@ -19,11 +19,17 @@ References:
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+
+ + restler
+   - https://github.com/danwrong/restler
+
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -35,6 +41,21 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+//var assertValidURL = function(url) {
+//  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+//    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+///    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+//    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+//    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+//    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+//  if(!pattern.test(url)) {
+//    console.log("Invalid URL : %s" , url);
+//    process.exit(1);
+//  } else {
+//    return url;
+//  }
+//}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -61,14 +82,39 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function(tempHtmlFile, checksFile) {
+    var urlToFile = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(result.message));
+        } else {
+            console.error("Writing url text to %s", tempHtmlFile);
+            fs.writeFileSync(tempHtmlFile, result);
+            var checkJson = checkHtmlFile(tempHtmlFile, checksFile);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+        }
+    };
+    return urlToFile;
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'url to check html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var htmlFileName = program.file;
+    if (program.url) {
+        var tempHtmlFileName = "tempIndex" + Math.floor((Math.random() * 10000) + 1).toString() + ".html";
+        var createHtmlFile = buildfn(tempHtmlFileName, program.checks);
+        rest.get(program.url).on('complete',createHtmlFile);
+        htmlFileName = tempHtmlFileName
+    } else {
+    
+        var checkJson = checkHtmlFile(htmlFileName, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
